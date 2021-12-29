4919539f3f6e36b20b7cf03d08f7c2928a20b7e2
Lucas Barga <lucasbarga@Lucass-MacBook-Pro.local>
2021-12-29 19:37:20 -0300

    remove trash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {
   QueryClient,
   QueryClientProvider
@@ -9,17 +8,6 @@ import List from './List';
 const queryClient = new QueryClient()
 
 const App = () => {
-  const [list, setList] = useState()
-
-  const getTestAxios = async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    setList(response.data)
-  }
-
-  useEffect(() => { getTestAxios() }, [])
-
-  console.log(list)
-
   return (
     <QueryClientProvider client={queryClient}>
       <List />
